0c47e03be99b45ba64a003471fd1fe8422eae3d4
akukrus <akukrus@kamaz.digital>
2025-11-07 07:33:01 +0300

    feature: getting a list of tasks and details for each task

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import PageTitle from "./ui/PageTitle";
+import TasksList from "./ui/TasksList";
+import TaskDetail from "./ui/TaskDetail";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(<MainPage />);
+
+function MainPage() {
+  const [taskId, setTaskId] = useState<string | null>(null);
+  const [boardId, setBoardId] = useState<string | null>(null);
+
+  const handleChangeTask = (tid: string, bid: string) => {
+    setTaskId(tid);
+    setBoardId(bid);
+  };
+  const handleResetTaskInfo = () => {
+    setTaskId(null);
+    setBoardId(null);
+  };
+  return (
+    <div>
+      <PageTitle />
+      <div style={{ display: "flex", gap: "30px" }}>
+        <TasksList
+          changeTask={handleChangeTask}
+          resetTaskInfo={handleResetTaskInfo}
+        />
+        <TaskDetail taskId={taskId} boardId={boardId} />
+      </div>
+    </div>
+  );
+}
